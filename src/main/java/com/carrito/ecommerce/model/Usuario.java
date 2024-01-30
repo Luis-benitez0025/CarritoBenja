97f098e73f7e6b6c6e2c468290ed6c4e76519119
Benjamin Merlini <benjaminmerlini@gmail.com>
2024-01-30 12:27:56 +0100

    base de datos usuario

--- src/main/java/com/carrito/ecommerce/model/Usuario.java
+++ src/main/java/com/carrito/ecommerce/model/Usuario.java
@@ -3,11 +3,35 @@ package com.carrito.ecommerce.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+
 public class Usuario {
-    private int idUser;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUser;
+
+    @Column(name = "nombreUsuario", nullable = false, length = 80)
     private String nombreUsuario;
+    @Column(name = "nombres", nullable = false, length = 150)
+    private String nombres;
+    @Column(name = "apellidos", nullable = false, length = 150)
+    private String apellidos;
+    @Column(name = "domicilio", nullable = false, length = 200)
+    private String domicilio;
+    @Column(name = "telefono", nullable = false, length = 10)
+    private String telefono;
+    @Column(name = "email", nullable = false, length = 150)
+    private String email;
+
+
 }
